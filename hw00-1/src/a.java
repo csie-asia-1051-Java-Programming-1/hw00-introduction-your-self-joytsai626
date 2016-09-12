f56703a13b1aa3f7592238fb80c13ebd6fe95eaa
joytsai626 <105021013@live.asia.edu.tw>
2016-09-12 15:54:25 +0800

    text
    text

---
+++ hw00-1/src/a.java
@@ -0,0 +1,13 @@
+
+public class a {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Tsai Meng-Hsuan\n105021013");
+
+	}
+
+}
